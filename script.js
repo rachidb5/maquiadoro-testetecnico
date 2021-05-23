9346b51854770fea1abf8289b9805e861ff272b9
rachidb5 <66842598+rachidb5@users.noreply.github.com>
2021-05-23 15:21:24 -0300

    ajuste nos links e texto de review

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const hour = document.getElementById('hora')
-
-
+const item = document.getElementsByClassName('item')
 
 function startTime()
 {
@@ -15,5 +14,17 @@ hour.innerHTML=`${h}:${m}`;
 t=setTimeout('startTime()',500);
 }
 
+function selectItem(event) {
+    const selectedElement = document.querySelector('.selected');
+    selectedElement.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
 
+function itemSelection() {
+    for(let i = 0; i < item.length; i += 1) {
+        item[i].addEventListener('click', selectItem)
+    }
+}
+
+itemSelection()
 startTime()
